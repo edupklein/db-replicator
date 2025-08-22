718ca7ed6a330b6c148ddad3045a7ecb34802e27
Eduardo Klein <eduardo.klein@sap.com>
2025-08-22 16:25:07 -0300

    Added Model classes

--- dbr-loader-app/src/main/java/com/dbrloaderapp/model/Client.java
+++ dbr-loader-app/src/main/java/com/dbrloaderapp/model/Client.java
@@ -1,4 +1,37 @@
 package com.dbrloaderapp.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "client")
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String email;
+    private String phone;
+    private String city;
+    private String state;
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+    // Getters and Setters
+    public Integer getId() {return id;}
+    public void setId(Integer id) {this.id = id;}
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
+    public String getPhone() {return phone;}
+    public void setPhone(String phone) {this.phone = phone;}
+    public String getCity() {return city;}
+    public void setCity(String city) {this.city = city;}
+    public String getState() {return state;}
+    public void setState(String state) {this.state = state;}
+    public LocalDateTime getCreationDate() {return creationDate;}
+    public void setCreationDate(LocalDateTime creationDate) {this.creationDate = creationDate;}
 }
--- dbr-loader-app/src/main/java/com/dbrloaderapp/model/User.java
+++ dbr-loader-app/src/main/java/com/dbrloaderapp/model/User.java
@@ -1,15 +1,37 @@
 package com.dbrloaderapp.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import java.lang.*;
+import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     private String username;
+    private String email;
+    private String password;
+    private String city;
+    private String state;
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+    // Getters and Setters
+    public Integer getId() {return id;}
+    public void setId(Integer id) {this.id = id;}
+    public String getUsername() {return username;}
+    public void setUsername(String username) {this.username = username;}
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
+    public String getPassword() {return password;}
+    public void setPassword(String password) {this.password = password;}
+    public String getCity() {return city;}
+    public void setCity(String city) {this.city = city;}
+    public String getState() {return state;}
+    public void setState(String state) {this.state = state;}
+    public LocalDateTime getCreationDate() {return creationDate;}
+    public void setCreationDate(LocalDateTime creationDate) {this.creationDate = creationDate;}
 }
